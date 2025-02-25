cede8c77e27c996c7915d44f72314d3b21514595
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-02-25 23:12:22 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        lua-language-server
+        nil
+      ];
+    };
+  };
+}
