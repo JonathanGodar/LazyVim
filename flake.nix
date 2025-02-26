fac99e1c5ace45b70fdb25be08f4fa00d130e7ec
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-02-26 23:21:18 +0100

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -3,25 +3,28 @@
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let 
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        lua-language-server
-        nil
-        pyright
-        bash-language-server
-        nushell
-        matlab-language-server
-        jdt-language-server
-        svelte-language-server
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          lua-language-server
+          nil
+          pyright
+          bash-language-server
+          nushell
+          matlab-language-server
+          jdt-language-server
+          nixfmt-rfc-style
+        ];
+      };
     };
-  };
 }
