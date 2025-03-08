fbcb1ea19d308d394b23eb6d292df03aad087a9c
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-03-08 23:26:18 +0100

    Add diffview lazy loading

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,6 +1,16 @@
 return {
   {
     "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewClose",
+      "DiffviewOpen",
+      "DiffviewFileHistory",
+      "DiffviewLog",
+      "DiffviewRefresh",
+      "DiffviewFocusFiles",
+      "DiffviewToggleFiles",
+    },
+
     -- keys = {
     --   ''
     -- },
