fbf14b480ded5321040a3d9c64ac2c310dc1d2f2
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-11-18 16:08:49 +0100

    Remove news popup

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+-- Disable news popup, Found using chatgpt
+vim.g.lazyvim_check_updates = false
+vim.g.lazyvim_news_enabled = false
+
 require("config.lazy")
